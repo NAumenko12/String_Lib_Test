7d1dd42b4763107de0a667da45ce6f11fc687909
NAumenko12 <143539153+NAumenko12@users.noreply.github.com>
2025-01-04 18:11:31 +0700

    Delete sscanf/s21_parse_count.c

--- sscanf/s21_parse_count.c
+++
@@ -1,9 +0,0 @@
-#include "s21_sscanf.h"
-
-static int parse_count(const char **str_ptr, va_list args) {
-    int *p = va_arg(args, int *);
-    if (p) {
-        *p = *str_ptr - str; 
-    }
-    return 0; 
-}
